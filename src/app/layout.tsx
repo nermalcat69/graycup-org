a8c0bbb3679897c28e865b3315ed45d885212ee0
BMO <meow@nermalcat69.dev>
2025-12-15 15:58:53 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,7 +87,7 @@ export default function RootLayout({
         )}
       >
         <RootProviders>{children}</RootProviders>
-        {/* <UserJotWidget /> */}
+        <UserJotWidget />
         <Script>
           {`(function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");
     script.src="${(process.env.NEXT_PUBLIC_CHATBASE_HOST || "https://www.chatbase.co/") + "embed.min.js"}";
